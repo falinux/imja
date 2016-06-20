cb6150549dadcec432f1371aa2f0c0f334baed91
이병복 <erunsee@falinux.com>
2016-06-20 20:38:37 +0900

    ach.sh -> make-imja-ramdisk.sh
    Former-commit-id: 803b1b778c9ef4a7fa28e911a2326c9c8e40450b

--- filesystem/ramdisk/make-imja-ramdisk.sh
+++ filesystem/ramdisk/make-imja-ramdisk.sh
@@ -4,6 +4,18 @@
 # 램디스크를 만든다. 
 #------------------------------------------
 RAMDISK=ramdisk.imx6-2.0-32M.gz
+TARGET_ROOT=target_ramdisk
+DIR_RAMDISK=dir_ramdisk
+ROOT_FILE=../../buildroot-2015.02/output/images/rootfs.tar
+
+
+if [-f ${ROOT_FILE}]; then
+   echo "tar xf ${ROOT_FILE} -C ${TARGET_ROOT}"
+   tar xf ${ROOT_FILE} -C ${TARGET_ROOT}
+else
+  echo "no such file ${ROOT_FILE}"
+  exit 1
+fi
 
 /sbin/losetup -d /dev/loop1
 rm -rf $RAMDISK
@@ -12,8 +24,16 @@ dd if=/dev/zero of=t_ramdisk bs=1k count=32768
 /sbin/mke2fs /dev/loop1 
 /sbin/losetup -d /dev/loop1
 mount -t ext2 -o loop t_ramdisk dir_ramdisk
-#ldconfig -r target_ramdiskroot
-cp -a target_ramdiskroot/* dir_ramdisk
+
+
+if [-d ${TARGET_ROOT}]; then
+    cp -a target_ramdiskroot/* dir_ramdisk
+else
+
+    mkdir ${TARGET_ROOT} ${DIR_RAMDISK}
+    cp -a ${TARGET_ROOT}/* ${DIR_RAMDISK}
+fi
+
 umount dir_ramdisk
 gzip t_ramdisk
 mv t_ramdisk.gz $RAMDISK 
